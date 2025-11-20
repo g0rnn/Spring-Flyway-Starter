24ed15e4b85b87b7f175947362ee52defb84aabe
gyunho <gyunh01@pukyong.ac.kr>
2025-11-20 20:57:34 +0900

    v3 sql

---
+++ src/main/resources/db/migration/V3__add_enrollment_indexes.sql
@@ -0,0 +1,6 @@
+CREATE INDEX idx_enrollments_student_id ON enrollments (student_id);
+
+CREATE INDEX idx_enrollments_course_id ON enrollments (course_id);
+
+CREATE INDEX idx_enrollments_status ON enrollments (status);
+
